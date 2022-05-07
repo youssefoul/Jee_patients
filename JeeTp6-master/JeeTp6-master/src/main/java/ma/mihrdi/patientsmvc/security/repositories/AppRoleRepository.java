fe54771d66c83a30133335cd7375f9a0195ceb99
youssef <oulouda.yousse1@gmail.com>
2022-05-07 15:06:54 +0000

    commit

--- JeeTp6-master/JeeTp6-master/src/main/java/ma/mihrdi/patientsmvc/security/repositories/AppRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.mihrdi.patientsmvc.security.repositories;
-
-import ma.mihrdi.patientsmvc.security.entities.AppRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppRoleRepository extends JpaRepository<AppRole,Long> {
-    AppRole findByRoleName(String roleName);
-}
--- JeeTp6-master/JeeTp6-master/src/main/java/ma/mihrdi/patientsmvc/security/repositories/AppUserRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.mihrdi.patientsmvc.security.repositories;
-
-import ma.mihrdi.patientsmvc.security.entities.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserRepository extends JpaRepository<AppUser,String> {
-    AppUser findByUsername(String username);
-}
